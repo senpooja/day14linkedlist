da19b14a69159ed2d2fc34e19c32733ea2906878
senpooja <poojasen744055@gmail.com>
2022-07-27 18:29:12 +0530

    uc-1

---
+++ uc-1-LinkedList/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello World!");
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+
+//list.InsertAtParticularPosition(2,30);
+list.Display();
+Console.ReadKey();
